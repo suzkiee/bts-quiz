f87c0a2a75cce5645b944fa2d3077ea1ab69cab2
Suzie K <suziekim.dev@gmail.com>
2021-02-06 19:31:00 -0500

    Adds Questions and logic for pushing answers

---
+++ index.js
@@ -0,0 +1,77 @@
+const quizContainer = document.getByElementId('quiz');
+const resultsContainer = document.getByElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "Typescript",
+      c: "npm"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: 'd'
+  }
+];
+
+function buildQuiz(){
+  //variable to store the HTML output
+  const output = [];
+
+  //for each question... iterative function
+  myQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+
+      //variable to store the list of possible answers
+      const answers = [];
+
+      // and for each avaialble answer
+      for (letter in currentQuestion.answers){
+        //.... add an HTML radio button
+        answers.push(
+          `<label>
+          <input type = "radio" name="question${questionNumber}" value="${letter}">
+          ${letter} :
+          ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+      // add this question and its answers to the output
+      output.push(
+        `<div class="questions"> ${currentQuestion.question} </div>`
+
+        `<div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+  // finally combine our output list into one string of HTML and put it on the page
+  quizContainer.innerHTML = output.join('');
+};
+
+function showResults(){
+
+};
+
+//display quiz right away
+buildQuiz();
+
+//on submit, show results
+submitButton.addEventListener('click', showResults);
