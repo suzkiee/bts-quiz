676380695dff31fa43d0ba2c293a591b3af16a38
Suzie K <suziekim.dev@gmail.com>
2021-02-13 19:28:33 -0500

    Adds JS for pagination

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
-  {
-    question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Sheryl Sandberg",
-      c: "Brendan Eich"
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: "Which one of these is a JavaScript package manager?",
-    answers: {
-      a: "Node.js",
-      b: "Typescript",
-      c: "npm"
-    },
-    correctAnswer: 'c'
-  },
-  {
-    question: "Which tool can you use to ensure code quality?",
-    answers: {
-      a: "Angular",
-      b: "jQuery",
-      c: "RequireJS",
-      d: "ESLint"
-    },
-    correctAnswer: 'd'
-  }
-];
-
 function buildQuiz(){
   //variable to store the HTML output
   const output = [];
@@ -56,9 +22,10 @@ function buildQuiz(){
       }
       // add this question and its answers to the output
       output.push(
-        `<div class="questions"> ${currentQuestion.question} </div>
-
-        <div class="answers"> ${answers.join('')} </div>`
+        `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
       );
     }
   );
@@ -98,8 +65,81 @@ function showResults(){
 resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.lenght}`;
 };
 
-//display quiz right away
-buildQuiz();
 
-//on submit, show results
+function showSlide(n){
+  slides[currentSlide].classList.remove('active-slide');
+  slides[n].classList.add('active-slide');
+  currentSlide = n;
+
+  if (currentSlide === 0){
+    previousButton.style.display = 'none';
+  }
+  else {
+    previousButton.style.display = 'inline-block';
+  };
+
+  if (currentSlide === slides.length-1){
+    nextButton.style.display = 'none';
+    submitButton.style.display = 'inline-block';
+  }
+  else {
+    nextButton.style.display = 'inline-block';
+    submitButton.style.display = 'none';
+  };
+};
+
+function showNextSlide(){
+  showSlide(currentSlide+1);
+}
+
+function showPreviousSlide(){
+  showSlide(currentSlide-1);
+}
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "Which one of these is a JavaScript package manager?",
+    answers: {
+      a: "Node.js",
+      b: "Typescript",
+      c: "npm"
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "Which tool can you use to ensure code quality?",
+    answers: {
+      a: "Angular",
+      b: "jQuery",
+      c: "RequireJS",
+      d: "ESLint"
+    },
+    correctAnswer: 'd'
+  }
+];
+//display quiz right away
+buildQuiz()
+
+//Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+showSlide(currentSlide);
+
+//on submit, show results, callback functions
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener('click', showPreviousSlide)
+nextButton.addEventListener('click', showNextSlide)
